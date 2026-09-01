f84465947ea85c0eed06faa9eaaa498870644691
Rafa M. <chuutasticaly@gmail.com>
2026-09-01 20:07:47 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -320,7 +320,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     <li><a href="habilidades-unicas.html">HABILIDADES ÚNICAS</a></li>
                     <li><a href="linhagens.html">LINHAGENS</a></li>
                     <li><a href="racas.html">RAÇAS</a></li>
-                    <li><a href="https://herikliz.github.io/New-Seas-RPG-Ficha/" target="_blank">SALVAR FICHA</a></li>
+                    <li><a href="https://persephonyyy.github.io/bobagensbobeiras/" target="_blank">SALVAR FICHA</a></li>
                 </ul>
             </li>
             <li>
